7d2060e8b8b32d15cd6b3ba28d06eeb877081e07
sailorneurotic <nicholaschanjj@gmail.com>
2020-09-29 22:30:10 +0800

    number of characters

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = '';
+
+  // var lineCounter = 0;
+
+  // while (lineCounter < input) {
+  var columnCounter = 0;
+
+  while (columnCounter < input) {
+    myOutputValue = myOutputValue + 'x';
+
+    columnCounter = columnCounter + 1;
+  }
   return myOutputValue;
 };
+
+//   //  // start a new line
+//   myOutputValue = myOutputValue + '<br>';
+
+//   lineCounter = lineCounter + 1;
+// }
